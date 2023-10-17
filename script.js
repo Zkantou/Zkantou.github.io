c57bc2e1c6f68959256600d77ac071f58952c38e
Zakaria Kantou <87882782+Zkantou@users.noreply.github.com>
2023-10-17 17:04:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["data analyst student", "Front-End Developer", "data analytics"],
+        strings: ["data analyst student", "Front-End Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["data analyst student", "Front-End Developer", "data analytics"],
+        strings: ["data analyst student", "Front-End Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
